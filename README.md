6f06f0ac17491c0fbd8af345a07f4bf62689d667
abhishek96negi <abhishek96negi@gmail.com>
2024-07-17 19:25:43 +0530

    circular linked list

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 1. [Singly Linked List](/scripts/Singly%20Linked%20List.py)
 2. [Doubly Linked List](/scripts/Doubly%20Linked%20List.py)
+3. [Circular Linkedin List](/scripts/Circular%20Linked%20List.py)
---
+++ scripts/Circular Linked List.py
@@ -0,0 +1,153 @@
+class Node:
+    def __init__(self, data, next_node=None):
+        """
+        Initialize a Node object with the given data.
+
+        Args:
+            data: The data to be stored in the node.
+            next_node (optional): The next node in the linked list. Defaults to None.
+        """
+        self.data = data
+        self.next = next_node
+
+class CLL:
+    def __init__(self):
+        """
+        Initialize a Circular Linked List object.
+        """
+        self.last = None
+
+    def is_empty(self):
+        """
+        is_empty method to check if the linked list is empty or not
+        """
+        return self.last is None
+
+    def insert_at_start(self, value):
+        """
+        Insert a node at the start of the linked list
+        Args:
+            value: data to be stored in the node
+        """
+        new_node = Node(value)
+
+        if self.is_empty():
+            new_node.next = new_node
+            self.last = new_node
+        else:
+            new_node.next = self.last.next
+            self.last.next = new_node
+
+    def insert_at_last(self, value):
+        """
+        Insert a node at the end of the linked list
+        Args:
+            value: data to be stored in the node
+        """
+        new_node = Node(value)
+        if self.is_empty():
+            new_node.next = new_node
+            self.last = new_node
+        else:
+            new_node.next = self.last.next
+            self.last.next = new_node
+            self.last = new_node
+
+    def search(self, value):
+        """
+        Search for a node with the given value in the linked list
+        Args:
+            value: data to be searched
+        Returns:
+            Node: node with the given value if found, else None
+        """
+        if self.is_empty():
+            return None
+
+        temp = self.last.next
+        while temp != self.last:
+            if temp.data == value:
+                return temp
+            temp = temp.next
+        if temp.data == value:
+            return temp
+        return None
+
+    def insert_after(self, value, temp):
+        if temp is None:
+            return None
+
+        new_node = Node(value, temp.next)
+        temp.next = new_node
+
+        if temp == self.last:
+            self.last = new_node
+
+    def print_list(self):
+        if not self.is_empty():
+            temp = self.last.next
+            while temp != self.last:
+                print(temp.data, end=' ')
+                temp = temp.next
+            print(temp.data)
+
+    def delete_first(self):
+
+        if not self.is_empty():
+            if self.last.next == self.last:
+                self.last = None
+            else:
+                self.last.next = self.last.next.next
+
+    def delete_last(self):
+        if not self.is_empty():
+            if self.last.next == self.last:
+                self.last = None
+            else:
+                temp = self.last.next
+                while temp.next != self.last:
+                    temp = temp.next
+                temp.next = self.last.next
+                self.last = temp
+
+    def delete_item(self, data):
+        if not self.is_empty():
+            if self.last.next == self.last:
+                if self.last.item == data:
+                    self.last = None
+            else:
+                if self.last.next.item == data:
+                    self.delete_first()
+                else:
+                    temp = self.last.next
+
+                    while temp != self.last:
+                        if temp.next == self.last:
+                            if self.last.item == data:
+                                self.delete_last()
+                            break
+                        if temp.next.item == data:
+                            temp.next = temp.next.next
+                            break
+                        temp = temp.next
+
+    def __iter__(self):
+
+        temp = self.last.next
+        while temp != self.last:
+            yield temp.data
+            temp = temp.next
+
+        yield temp.data
+
+cll = CLL()
+cll.insert_at_start(10)
+cll.insert_at_start(20)
+cll.insert_at_last(30)
+cll.insert_at_last(40)
+cll.insert_after(50, cll.search(10))
+# 20 10 50 30 40
+for x in cll:
+    print(x, end=' ')
+print()
+cll.print_list()
