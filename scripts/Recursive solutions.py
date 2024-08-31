69853fde8870333a6a35b5982a2697c66e49ef76
abhishek96negi <abhishek96negi@gmail.com>
2024-08-31 19:41:48 +0530

    recursive solutions

--- scripts/Recursive solutions.py
+++ scripts/Recursive solutions.py
@@ -33,5 +33,30 @@ def printNEvenreverse(n):
         print(2 * n, end=' ')
         printNEvenreverse(n - 1)
 
+def sumN(n):
+    if n==1:
+        return 1
+    return n + sumN(n-1)
 
+def sumNOdd(n):
+    if n==1:
+        return 1
+    return 2*n-1 + sumNOdd(n-1)
+
+def sumNEven(n):
+    if n==1:
+        return 2
+    return 2*n + sumNEven(n-1)
+
+def fact(n):
+    if n==0:
+        return 1
+    return n * fact(n-1)
+
+def sumNSquare(n):
+    if n==1:
+        return 1
+    return n*n + sumNSquare(n-1)
+
+print("Sum is",sumNSquare(5))
 printNOddreverse(10)
